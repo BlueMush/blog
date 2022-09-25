6627fefd6e5521c03b4aaafa7904bd6405f1d739
BlueMush <gusah0909@naver.com>
2022-09-25 15:17:56 +0900

    bootstrap 적용

---
+++ src/main/java/com/example/blog/main/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.example.blog.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+    @RequestMapping("/main")
+    public String main(){
+        return "index";
+    }
+
+    @RequestMapping(value="/main2", method = RequestMethod.GET)
+    public String main2(){
+        return "index";
+    }
+}
